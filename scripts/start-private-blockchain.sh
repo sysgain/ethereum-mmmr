0ecda67deca7c32954d4849b2c2d204fb4f06804
PradeepTS <PradeepTS>
2017-08-31 18:09:28 +0530

    updated rbnodes

--- scripts/start-private-blockchain.sh
+++ scripts/start-private-blockchain.sh
@@ -52,7 +52,7 @@ echo "bootnode_urls are: $BOOTNODE_URLS"
 ############################################################
 # Make boot node urls available to other consortium members
 ############################################################
-bootnode_urls_data="{\"remoteBootNodeUrls\":\"${BOOTNODE_URLS}\"}"
+bootnode_urls_data="{\"id\":\"rbnodes\",\"remoteBootNodeUrls\":\"${BOOTNODE_URLS}\"}"
 if [ $NODE_TYPE -eq 0 ]; then
   sh getpost-utility.sh $masterkey "${endpointurl}dbs/${dbname}/colls/${collname}/docs" "post" "$bootnode_urls_data"
 fi
