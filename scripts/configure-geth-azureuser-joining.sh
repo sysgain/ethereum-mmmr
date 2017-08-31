43dda7656b4e71c5165fd3cf711182cdfe352fb3
PradeepTS <pradeepts@sysgain.com>
2017-08-31 12:25:48 +0530

    Update configure-geth-azureuser-joining.sh

--- scripts/configure-geth-azureuser-joining.sh
+++ scripts/configure-geth-azureuser-joining.sh
@@ -80,7 +80,7 @@ echo $BOOTNODE_URLS
 #########################################
 wget -N ${REMOTE_BOOTNODE_URL} || exit 1;
 IP_TO_PING=$(grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' bootnodes.txt | head -1)
-
+echo "IP_TO_PING is: $IP_TO_PING"
 REMOTE_BOOTNODE_URLS=`cat bootnodes.txt`;
 BOOTNODE_URLS="${BOOTNODE_URLS} ${REMOTE_BOOTNODE_URLS}";
 
