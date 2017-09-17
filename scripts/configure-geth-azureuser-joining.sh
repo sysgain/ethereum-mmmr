c3255ee9b8dfd49cf4e740f44199289d81ec0ab0
Ashwin Sebastian <asebastian@sysgain.com>
2017-09-17 20:48:32 +0530

    Update configure-geth-azureuser-joining.sh

--- scripts/configure-geth-azureuser-joining.sh
+++ scripts/configure-geth-azureuser-joining.sh
@@ -105,7 +105,7 @@ while sleep 5; do
         allremotedocs=`sh getpost-utility.sh $remotedocdbprimarykey "${remoteendpointurl}dbs/${remotedbname}/colls/${remotecollname}/docs" get`
         echo "allRemotedocs: $allremotedocs"
         hostcount=`echo $allremotedocs | grep -Po '"bootNodeUrlNode":.*?",' | cut -d "," -f1 | cut -d '"' -f4 | wc -l`
-        if [ $hostcount -gt $nodecount ]; then
+        if [ $hostcount -ge $nodecount ]; then
            break
         fi
 done
